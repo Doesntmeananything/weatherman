c593c8613a11cae3be74dfd312d96228c714e3a0
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-01-26 11:38:52 +0300

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Layout } from "./Layout";
+
+const App = () => (
+  <ThemeProvider>
+    <CSSReset />
+    <Layout />
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/Layout.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+
+export const Layout = () => <Button>Some button text</Button>;
