1c415e96eef95285e92ed7187f46ac69875299fc
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-02-09 20:26:00 +0300

    Add weather condition icon

--- src/ForecastPanel.tsx
+++ src/ForecastPanel.tsx
@@ -1,25 +1,49 @@
-import React from "react";
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/core";
+import React, { useContext } from "react";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Stack,
+  Text,
+  Box,
+  Image
+} from "@chakra-ui/core";
 import { fromUnixTime, format } from "date-fns";
 
 import weatherData from "./mockWeatherAPI.json";
+import { OptionsContext } from "./weatherOptionsContext";
+
+const forecastDays = weatherData.forecast.forecastday;
 
 export const ForecastPanel = () => {
-  const forecastDays = weatherData.forecast.forecastday;
+  const { scale } = useContext(OptionsContext);
+  const avgTemp = (day: { avgtemp_c: number; avgtemp_f: number }) =>
+    scale === "C" ? `${day.avgtemp_c} °C` : `${day.avgtemp_f} °F`;
 
   return (
     <Tabs variant="enclosed">
       <TabList>
-        {forecastDays.map(({ date_epoch }) => (
+        {forecastDays.map(({ date_epoch, day }) => (
           <Tab key={date_epoch}>
-            {format(fromUnixTime(date_epoch), "EEE d")}
+            <Stack>
+              <Text>{format(fromUnixTime(date_epoch), "EEE d")}</Text>
+              <Image
+                src={day.condition.icon}
+                alt={day.condition.text}
+                size="46px"
+              />
+              <Box>{avgTemp(day)}</Box>
+            </Stack>
           </Tab>
         ))}
       </TabList>
 
       <TabPanels>
-        <TabPanel>one!</TabPanel>
-        <TabPanel>two!</TabPanel>
+        {forecastDays.map(({ date, day }) => (
+          <TabPanel key={date}>{day.mintemp_c}</TabPanel>
+        ))}
       </TabPanels>
     </Tabs>
   );
