875746ee32912345608b3c3c52d79d44cae40d23
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-02-19 19:14:27 +0300

    Add app name to header

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { useColorMode, Box, IconButton } from "@chakra-ui/core";
+import {
+  useColorMode,
+  Box,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+  Flex,
+  Button
+} from "@chakra-ui/core";
+import { DiGithubBadge } from "react-icons/di";
 
 import { WeatherLocationSearcher } from "./WeatherWidget/WeatherLocationSearcher";
 
@@ -14,18 +24,35 @@ export const Layout = () => {
         d="flex"
         justifyContent="flex-end"
       >
-        <IconButton
-          aria-label={`Switch to ${
-            colorMode === "light" ? "dark" : "light"
-          } mode`}
-          variant="ghost"
-          m={2}
-          fontSize="20px"
-          onClick={toggleColorMode}
-          icon={colorMode === "light" ? "moon" : "sun"}
-        >
-          Toggle {colorMode === "light" ? "Dark" : "Light"}
-        </IconButton>
+        <Flex size="100%" px="6" align="center" justify="space-around">
+          <Button variant="ghost">
+            <a href="/">
+              <Text fontSize="xl">Weatherman</Text>
+            </a>
+          </Button>
+          <Flex align="center">
+            <Stack align="center" isInline spacing={3}>
+              <Link
+                isExternal
+                href="https://github.com/Doesntmeananything/weatherman"
+              >
+                <Box as={DiGithubBadge} size="8" color="current" />
+              </Link>
+            </Stack>
+            <IconButton
+              aria-label={`Switch to ${
+                colorMode === "light" ? "dark" : "light"
+              } mode`}
+              variant="ghost"
+              m={2}
+              fontSize="20px"
+              onClick={toggleColorMode}
+              icon={colorMode === "light" ? "moon" : "sun"}
+            >
+              Toggle {colorMode === "light" ? "Dark" : "Light"}
+            </IconButton>
+          </Flex>
+        </Flex>
       </Box>
       <Box
         as="main"
